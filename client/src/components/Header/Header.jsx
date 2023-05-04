891c973c86d7dca33738a4d7f2a34157fd9ced05
Fatmanur Gebeşoğlu <76436684+FatmanurGebesoglu@users.noreply.github.com>
2023-05-04 19:47:49 +0300

    Header component edited

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,6 +1,14 @@
 import React from "react";
-import { Input } from "antd";
-import { SearchOutlined, HomeOutlined, ShoppingCartOutlined, CopyOutlined,UserOutlined,BarChartOutlined,LogoutOutlined  } from "@ant-design/icons";
+import { Badge, Input } from "antd";
+import {
+  SearchOutlined,
+  HomeOutlined,
+  ShoppingCartOutlined,
+  CopyOutlined,
+  UserOutlined,
+  BarChartOutlined,
+  LogoutOutlined,
+} from "@ant-design/icons";
 
 const Header = () => {
   return (
@@ -11,7 +19,7 @@ const Header = () => {
             <h2 className="text-2xl font-bold md:text-4xl">LOGO</h2>
           </a>
         </div>
-        <div className="header-search flex-1">
+        <div className="header-search flex-1 flex justify-center">
           <Input
             size="large"
             placeholder="Ürün Ara..."
@@ -19,32 +27,61 @@ const Header = () => {
             className="rounded-full max-w-[800px]"
           />
         </div>
-        <div className="menu-links flex justify-between items-center gap-8">
-          <a href={"/"} className="flex flex-col">
+        <div className="menu-links flex justify-between items-center gap-7 md:static fixed bottom-0 md:w-auto w-screen md:bg-transparent bg-white left-0 md:border-t-0 border-t md:px-0 px-4 py-1 ">
+          <a
+            href={"/"}
+            className="flex flex-col hover:text-[#40a9ff] transition-all"
+          >
             <HomeOutlined className="md:text-2xl text-xl" />
             <span className="md:text-xs text-[12px]">Anasayfa</span>
           </a>
-          <a href={"/"} className="flex flex-col">
-            <ShoppingCartOutlined  className="md:text-2xl text-xl" />
-            <span className="md:text-xs text-[12px]">Sepet</span>
-          </a>
-          <a href={"/"} className="flex flex-col">
+          <Badge count={5} offset={[0,6]} className="md:flex hidden">
+            <a
+              href={"/"}
+              className="flex flex-col hover:text-[#40a9ff] transition-all "
+            >
+              <ShoppingCartOutlined className="md:text-2xl text-xl" />
+              <span className="md:text-xs text-[12px]">Sepet</span>
+            </a>
+          </Badge>
+          <a
+            href={"/"}
+            className="flex flex-col hover:text-[#40a9ff] transition-all"
+          >
             <CopyOutlined className="md:text-2xl text-xl" />
             <span className="md:text-xs text-[12px]">Faturalar</span>
           </a>
-          <a href={"/"} className="flex flex-col">
+          <a
+            href={"/"}
+            className="flex flex-col hover:text-[#40a9ff] transition-all"
+          >
             <UserOutlined className="md:text-2xl text-xl" />
             <span className="md:text-xs text-[12px]">Müşteriler</span>
           </a>
-          <a href={"/"} className="flex flex-col">
+          <a
+            href={"/"}
+            className="flex flex-col hover:text-[#40a9ff] transition-all"
+          >
             <BarChartOutlined className="md:text-2xl text-xl" />
             <span className="md:text-xs text-[12px]">İstatistikler</span>
           </a>
-          <a href={"/"} className="flex flex-col">
+          <a
+            href={"/"}
+            className="flex flex-col hover:text-[#40a9ff] transition-all"
+          >
             <LogoutOutlined className="md:text-2xl text-xl" />
             <span className="md:text-xs text-[12px]">Çıkış</span>
           </a>
         </div>
+        <Badge count={5} offset={[0,6]} className="md:hidden flex">
+            <a
+              href={"/"}
+              className="flex flex-col hover:text-[#40a9ff] transition-all "
+            >
+              <ShoppingCartOutlined className="text-2xl" />
+              <span className="md:text-xs text-[12px]">Sepet</span>
+            </a>
+          </Badge>
       </header>
     </div>
   );
