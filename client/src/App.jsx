f62bcf0aba848fdb1b7e30f443569a204e96c2bd
Fatmanur Gebeşoğlu <76436684+FatmanurGebesoglu@users.noreply.github.com>
2023-05-05 10:51:48 +0300

    home page edited

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,22 @@
+import React from "react";
 import Header from "./components/Header/Header";
 
 function App() {
   return (
-    <div>
+    <React.Fragment>
       <Header/>
-      home
-    </div>
+      <div className="home px-6 flex justify-between">
+        <div className="categories">
+          <div>categories</div>
+        </div>
+        <div className="products">
+          <div>products</div>
+        </div>
+        <div className="cart">
+          <div>cart totals</div>
+        </div>
+      </div>
+    </React.Fragment>
   );
 }
 
