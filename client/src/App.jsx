895e854ebcf2214bcc7f718c3d6402ce6dbf644a
Fatmanur Gebeşoğlu <76436684+FatmanurGebesoglu@users.noreply.github.com>
2023-05-05 15:35:24 +0300

    CartTotals component created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,20 +2,21 @@ import React from "react";
 import Header from "./components/Header/Header";
 import Categories from "./components/Categories/Categories";
 import Products from "./components/Products/Products";
+import CartTotals from "./components/Cart/CartTotals";
 
 function App() {
   return (
     <React.Fragment>
       <Header/>
       <div className="home px-6 flex justify-between gap-10">
-        <div className="categories flex-1 overflow-auto max-h-[calc(100vh-_-112px)] pb-64">
+        <div className="categories overflow-auto max-h-[calc(100vh-_-112px)] pb-64">
           <Categories />
         </div>
         <div className="products flex-[8]">
           <Products/>
         </div>
-        <div className="cart">
-          <div>cart totals</div>
+        <div className="cart-wrapper min-w-[300px] md:-mr-[24px] md:-mt-[24px] border">
+          <CartTotals/>
         </div>
       </div>
     </React.Fragment>
--- client/src/components/Categories/Categories.jsx
+++ client/src/components/Categories/Categories.jsx
@@ -3,31 +3,31 @@ import React from "react";
 const Categories = () => {
   return (
     <ul className="flex gap-4 flex-col text-lg">
-      <li className="bg-slate-800 px-6 py-10 text-white cursor-pointer hover:bg-orange-600 transition-all text-center">
+      <li className="bg-slate-800 px-6 py-10 text-white cursor-pointer hover:bg-orange-600 transition-all text-center min-w-[145px]">
         <span>Tümü</span>
       </li>
-      <li className="bg-slate-800 px-6 py-10 text-white cursor-pointer hover:bg-orange-600 transition-all text-center">
+      <li className="bg-slate-800 px-6 py-10 text-white cursor-pointer hover:bg-orange-600 transition-all text-center min-w-[145px]">
         <span>Yiyecek</span>
       </li>
-      <li className="bg-slate-800 px-6 py-10 text-white cursor-pointer hover:bg-orange-600 transition-all text-center">
+      <li className="bg-slate-800 px-6 py-10 text-white cursor-pointer hover:bg-orange-600 transition-all text-center min-w-[145px]">
         <span>İçecek</span>
       </li>
-      <li className="bg-slate-800 px-6 py-10 text-white cursor-pointer hover:bg-orange-600 transition-all text-center">
+      <li className="bg-slate-800 px-6 py-10 text-white cursor-pointer hover:bg-orange-600 transition-all text-center min-w-[145px]">
         <span>Meyve</span>
       </li>
-      <li className="bg-slate-800 px-6 py-10 text-white cursor-pointer hover:bg-orange-600 transition-all text-center">
+      <li className="bg-slate-800 px-6 py-10 text-white cursor-pointer hover:bg-orange-600 transition-all text-center min-w-[145px]">
         <span>Meyve</span>
       </li>
-      <li className="bg-slate-800 px-6 py-10 text-white cursor-pointer hover:bg-orange-600 transition-all text-center">
+      <li className="bg-slate-800 px-6 py-10 text-white cursor-pointer hover:bg-orange-600 transition-all text-center min-w-[145px]">
         <span>Meyve</span>
       </li>
-      <li className="bg-slate-800 px-6 py-10 text-white cursor-pointer hover:bg-orange-600 transition-all text-center">
+      <li className="bg-slate-800 px-6 py-10 text-white cursor-pointer hover:bg-orange-600 transition-all text-center min-w-[145px]">
         <span>Meyve</span>
       </li>
-      <li className="bg-slate-800 px-6 py-10 text-white cursor-pointer hover:bg-orange-600 transition-all text-center">
+      <li className="bg-slate-800 px-6 py-10 text-white cursor-pointer hover:bg-orange-600 transition-all text-center min-w-[145px]">
         <span>Meyve</span>
       </li>
-      <li className="bg-slate-800 px-6 py-10 text-white cursor-pointer hover:bg-orange-600 transition-all text-center">
+      <li className="bg-slate-800 px-6 py-10 text-white cursor-pointer hover:bg-orange-600 transition-all text-center min-w-[145px]">
         <span>Meyve</span>
       </li>
     </ul>
