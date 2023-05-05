d2873468ef46dcebbcba7965a18a9a4e1c85b6b8
Fatmanur Gebeşoğlu <76436684+FatmanurGebesoglu@users.noreply.github.com>
2023-05-05 11:36:58 +0300

    categories component created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,16 @@
 import React from "react";
 import Header from "./components/Header/Header";
+import Categories from "./components/Categories/Categories";
 
 function App() {
   return (
     <React.Fragment>
       <Header/>
-      <div className="home px-6 flex justify-between">
-        <div className="categories">
-          <div>categories</div>
+      <div className="home px-6 flex justify-between gap-10">
+        <div className="categories flex-1 overflow-auto max-h-[calc(100vh-_-112px)] pb-64">
+          <Categories />
         </div>
-        <div className="products">
+        <div className="products flex-[8]">
           <div>products</div>
         </div>
         <div className="cart">
