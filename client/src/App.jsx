9daff8f289e51c3a98d1f637c2ce7ded97387161
Fatmanur Gebeşoğlu <76436684+FatmanurGebesoglu@users.noreply.github.com>
2023-05-06 16:26:29 +0300

    React Router Dom added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,26 +1,15 @@
 import React from "react";
-import Header from "./components/Header/Header";
-import Categories from "./components/Categories/Categories";
-import Products from "./components/Products/Products";
-import CartTotals from "./components/Cart/CartTotals";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
 
 function App() {
   return (
-    <React.Fragment>
-      <Header/>
-      <div className="home px-6 flex md:flex-row flex-col justify-between gap-10 md:pb-0 pb-24">
-        <div className="categories overflow-auto max-h-[calc(100vh_-_112px)] md:pb-10">
-          <Categories />
-        </div>
-        <div className="products flex-[8] max-h-[calc(100vh_-_112px)] overflow-y-auto pb-10">
-          <Products/>
-        </div>
-        <div className="cart-wrapper min-w-[300px] md:-mr-[24px] md:-mt-[24px] border ">
-          <CartTotals/>
-        </div>
-      </div>
-    </React.Fragment>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </BrowserRouter>
   );
-}
+} 
 
 export default App;
