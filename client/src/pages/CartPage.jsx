7cd2fa3976a6a039df5a8ab09910b4c3808435e8
Fatmanur Gebeşoğlu <76436684+FatmanurGebesoglu@users.noreply.github.com>
2023-05-06 18:49:34 +0300

    CreateBill Modal created

--- client/src/pages/CartPage.jsx
+++ client/src/pages/CartPage.jsx
@@ -1,66 +1,83 @@
-import React from "react";
-import Header from "../components/Header/Header";
-import { Table, Card, Button } from "antd";
-
-const dataSource = [
-  {
-    key: "1",
-    name: "Mike",
-    age: 32,
-    address: "10 Downing Street",
-  },
-  {
-    key: "2",
-    name: "John",
-    age: 42,
-    address: "10 Downing Street",
-  },
-];
-
-const columns = [
-  {
-    title: "Name",
-    dataIndex: "name",
-    key: "name",
-  },
-  {
-    title: "Age",
-    dataIndex: "age",
-    key: "age",
-  },
-  {
-    title: "Address",
-    dataIndex: "address",
-    key: "address",
-  },
-];
+import { Button, Card, Table } from "antd";
+import { useState } from "react";
+import CreateBill from "../components/Cart/CreateBill.jsx";
+import Header from "../components/Header/Header.jsx";
 
 const CartPage = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const dataSource = [
+    {
+      key: "1",
+      name: "Mike",
+      age: 32,
+      address: "10 Downing Street",
+    },
+    {
+      key: "2",
+      name: "John",
+      age: 42,
+      address: "10 Downing Street",
+    },
+  ];
+  const columns = [
+    {
+      title: "Name",
+      dataIndex: "name",
+      key: "name",
+    },
+    {
+      title: "Age",
+      dataIndex: "age",
+      key: "age",
+    },
+    {
+      title: "Address",
+      dataIndex: "address",
+      key: "address",
+    },
+  ];
+
+  console.log(isModalOpen);
+
   return (
-    <div>
+    <>
       <Header />
       <div className="px-6">
-        <Table dataSource={dataSource} columns={columns} bordered pagination={false} />
+        <Table
+          dataSource={dataSource}
+          columns={columns}
+          bordered
+          pagination={false}
+        />
         <div className="cart-total flex justify-end mt-4">
           <Card className="w-72">
             <div className="flex justify-between">
               <span>Ara Toplam</span>
-              <span>102.50₺</span>
+              <span>549.00₺</span>
             </div>
             <div className="flex justify-between my-2">
               <span>KDV Toplam %8</span>
-              <span className="text-red-600">+8.72₺</span>
+              <span className="text-red-600">+43.92₺</span>
             </div>
             <div className="flex justify-between">
-              <b>Toplam </b>
-              <b>111.22₺</b>
+              <b>Toplam</b>
+              <b>592.92₺</b>
             </div>
-            <Button className="mt-4 w-full" type="primary" size="large">Sipariş Oluştur</Button>
+            <Button
+              className="mt-4 w-full"
+              type="primary"
+              size="large"
+              onClick={() => setIsModalOpen(true)}
+            >
+              Sipariş Oluştur
+            </Button>
           </Card>
         </div>
       </div>
-    </div>
+      <CreateBill isModalOpen={isModalOpen} setIsModalOpen={setIsModalOpen} />
+    </>
   );
 };
+
 
 export default CartPage;
