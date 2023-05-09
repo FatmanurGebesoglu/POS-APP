b9772355f4711e6326271209411d9fcebee0d6dd
Fatmanur Gebeşoğlu <76436684+FatmanurGebesoglu@users.noreply.github.com>
2023-05-09 19:29:01 +0300

    PrintBill component for page size

--- client/src/components/Bills/PrintBill.jsx
+++ client/src/components/Bills/PrintBill.jsx
@@ -1,175 +1,230 @@
-import { Modal, Button} from "antd";
+import { Modal, Button } from "antd";
 
 const PrintBill = ({ isModalOpen, setIsModalOpen }) => {
-
-  
-
   return (
     <Modal
-    title="Fatura Yazdır"
-    open={isModalOpen}
-    footer={false}
-    onCancel={() => setIsModalOpen(false)}
-    width={800}
-  >
-    <section className="py-20 bg-black">
-      <div className="max-w-5xl mx-auto bg-white px-6">
-        <article className="overflow-hidden">
-          <div className="logo my-6">
-            <h2 className="text-4xl font-bold text-slate-700">LOGO</h2>
-          </div>
-          <div className="bill-details">
-            <div className="grid grid-cols-4 gap-12">
-              <div className="text-md text-slate-500">
-                <p className="font-bold text-slate-700">Fatura Detayı:</p>
-                <p>Karabük</p>
-                <p> Kılavuzlar Mahallesi</p>
-                <p> Demirçelik Kampüsü </p>
-                <p> Mühendislik Fakültesi</p>
-              </div>
-              <div className="text-md text-slate-500">
-                <p className="font-bold text-slate-700">Fatura:</p>
-                The Logo Company
-                <p> Karabük Teknokent Binası</p>
-                <p> Karabük </p>
-                <p> 78100</p>
-              </div>
-              <div className="text-md text-slate-500">
-                <div>
-                  <p className="font-bold text-slate-700">Fatura numarası:</p>
-                  <p>00026</p>
+      title="Fatura Yazdır"
+      open={isModalOpen}
+      footer={false}
+      onCancel={() => setIsModalOpen(false)}
+      width={800}
+    >
+      <section className="py-20 bg-black">
+        <div className="max-w-5xl mx-auto bg-white px-6">
+          <article className="overflow-hidden">
+            <div className="logo my-6">
+              <h2 className="text-4xl font-bold text-slate-700">LOGO</h2>
+            </div>
+            <div className="bill-details">
+              <div className="grid sm:grid-cols-4 grid-cols-3 gap-12">
+                <div className="text-md text-slate-500 sm:block hidden">
+                  <p className="font-bold text-slate-700">Fatura Detayı:</p>
+                  <p>Karabük</p>
+                  <p> Kılavuzlar Mahallesi</p>
+                  <p> Demirçelik Kampüsü </p>
+                  <p> Mühendislik Fakültesi</p>
                 </div>
-                <div>
-                  <p className="font-bold text-slate-700 mt-2">
-                    Veriliş Tarihi:
+                <div className="text-md text-slate-500">
+                  <p className="font-bold text-slate-700">Fatura:</p>
+                  The Logo Company
+                  <p> Karabük Teknokent Binası</p>
+                  <p> Karabük </p>
+                  <p> 78100</p>
+                </div>
+                <div className="text-md text-slate-500">
+                  <div>
+                    <p className="font-bold text-slate-700">Fatura numarası:</p>
+                    <p>00026</p>
+                  </div>
+                  <div>
+                    <p className="font-bold text-slate-700 mt-2">
+                      Veriliş Tarihi:
+                    </p>
+                    <p>2023-05-09</p>
+                  </div>
+                </div>
+                <div className="text-md text-slate-500">
+                  <div>
+                    <p className="font-bold text-slate-700">Şartlar:</p>
+                    <p>15 gün</p>
+                  </div>
+                  <div>
+                    <p className="font-bold text-slate-700 mt-2">Vade:</p>
+                    <p>2024-06-09</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="bill-table-area mt-8">
+              <table className="min-w-full divide-y divide-slate-500 overflow-hidden">
+                <thead>
+                  <tr className="border-b border-slate-200">
+                    <th
+                      scope="col"
+                      className="py-3.5 text-left text-sm font-normal text-slate-700 md:pl-0 sm:table-cell hidden"
+                    >
+                      Görsel
+                    </th>
+                    <th
+                      scope="col"
+                      className="py-3.5 text-left text-sm font-normal text-slate-700 md:pl-0 sm:table-cell hidden"
+                    >
+                      {" "}
+                      Başlık
+                    </th>
+                    <th
+                      colSpan={4}
+                      scope="col"
+                      className="py-3.5 text-left text-sm font-normal text-slate-700 md:pl-0 sm:hidden"
+                    >
+                      {" "}
+                      Başlık
+                    </th>
+                    <th
+                      scope="col"
+                      className="py-3.5 text-center text-sm font-normal text-slate-700 md:pl-0 sm:table-cell hidden"
+                    >
+                      Fiyat
+                    </th>
+                    <th
+                      scope="col"
+                      className="py-3.5 text-center text-sm font-normal text-slate-700 md:pl-0 sm:table-cell hidden"
+                    >
+                      Adet
+                    </th>
+                    <th
+                      scope="col"
+                      className="py-3.5 text-end text-sm font-normal text-slate-700 md:pl-0"
+                    >
+                      Toplam
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr className="border-b border-slate-200">
+                    <td className="py-4 sm:table-cell hidden ">
+                      <img
+                        src="https://parade.com/.image/t_share/MTkwNTgxNDY1MzcxMTkxMTY0/different-types-of-apples-jpg.jpg"
+                        alt=""
+                        className="w-12 h-12 object-cover"
+                      />
+                    </td>
+                    <td className="py-4 sm:table-cell hidden">
+                      <div className="flex flex-col">
+                        <span className="font-medium">Şalgam</span>
+                        <span className="sm:hidden inline-block text-xs">
+                          Birim Fiyatı 5₺
+                        </span>
+                      </div>
+                    </td>
+                    <td className="py-4 sm:hidden" colSpan={4}>
+                      <div className="flex flex-col">
+                        <span className="font-medium">Elma</span>
+                        <span className="sm:hidden inline-block text-xs">
+                          Birim Fiyatı 5₺
+                        </span>
+                      </div>
+                    </td>
+                    <td className="py-4 text-center sm:table-cell hidden">
+                      <span>16.00₺</span>
+                    </td>
+                    <td className="py-4 sm:text-center text-right sm:table-cell hidden">
+                      <span>1</span>
+                    </td>
+                    <td className="py-4 text-end">
+                      <span>16.00₺</span>
+                    </td>
+                  </tr>
+                </tbody>
+                <tfoot>
+                  <tr>
+                    <th
+                      className="text-right pt-4 sm:table-cell hidden"
+                      colSpan="4"
+                      scope="row"
+                    >
+                      <span className="font-normal text-slate-700">
+                        Ara Toplam
+                      </span>
+                    </th>
+                    <th
+                      className="text-left pt-4 sm:hidden"
+                      scope="row"
+                      colSpan="4"
+                    >
+                      <p className="font-normal text-slate-700">Ara Toplam</p>
+                    </th>
+                    <th className="text-right pt-4" scope="row">
+                      <span className="font-normal text-slate-700">61₺</span>
+                    </th>
+                  </tr>
+                  <tr>
+                    <th
+                      className="text-right pt-4 sm:table-cell hidden"
+                      colSpan="4"
+                      scope="row"
+                    >
+                      <span className="font-normal text-slate-700">KDV</span>
+                    </th>
+                    <th
+                      className="text-left pt-4 sm:hidden"
+                      scope="row"
+                      colSpan="4"
+                    >
+                      <p className="font-normal text-slate-700">KDV</p>
+                    </th>
+                    <th className="text-right pt-4" scope="row">
+                      <span className="font-normal text-red-600">+4.88₺</span>
+                    </th>
+                  </tr>
+                  <tr>
+                    <th
+                      className="text-right pt-4 sm:table-cell hidden"
+                      colSpan="4"
+                      scope="row"
+                    >
+                      <span className="font-normal text-slate-700">
+                        Genel Toplam
+                      </span>
+                    </th>
+                    <th
+                      className="text-left pt-4 sm:hidden"
+                      scope="row"
+                      colSpan="4"
+                    >
+                      <p className="font-normal text-slate-700">Genel Toplam</p>
+                    </th>
+                    <th className="text-right pt-4" scope="row">
+                      <span className="font-normal text-slate-700">65.88₺</span>
+                    </th>
+                  </tr>
+                </tfoot>
+              </table>
+              <div className="py-9">
+                <div className="border-t pt-9 border-slate-200">
+                  <p className="text-sm font-light text-slate-700">
+                    Ödeme koşulları 14 gündür. Paketlenmemiş Borçların Geç
+                    Ödenmesi Yasası 0000'e göre, serbest çalışanların bu süreden
+                    sonra borçların ödenmemesi durumunda 00.00 gecikme ücreti
+                    talep etme hakkına sahip olduklarını ve bu noktada bu ücrete
+                    ek olarak yeni bir fatura sunulacağını lütfen unutmayın.
+                    Revize faturanın 14 gün içinde ödenmemesi durumunda, vadesi
+                    geçmiş hesaba ek faiz ve %8 yasal oran artı %0,5 Bank of
+                    England tabanı olmak üzere toplam %8,5 uygulanacaktır.
+                    Taraflar Kanun hükümleri dışında sözleşme yapamazlar.
                   </p>
-                  <p>2023-05-09</p>
-                </div>
-              </div>
-              <div className="text-md text-slate-500">
-                <div>
-                  <p className="font-bold text-slate-700">Şartlar:</p>
-                  <p>15 gün</p>
-                </div>
-                <div>
-                  <p className="font-bold text-slate-700 mt-2">Vade:</p>
-                  <p>2024-06-09</p>
                 </div>
               </div>
             </div>
-          </div>
-          <div className="bill-table-area mt-8">
-            <table className="min-w-full divide-y divide-slate-500 overflow-hidden">
-              <thead>
-                <tr className="border-b border-slate-200">
-                  <th
-                    scope="col"
-                    className="py-3.5 pl-4 text-left text-sm font-normal text-slate-700 sm:pl-6 md:pl-0 sm:table-cell hidden"
-                  >
-                    Görsel
-                  </th>
-                  <th
-                    scope="col"
-                    className="py-3.5 pl-4 text-left text-sm font-normal text-slate-700 sm:pl-6 md:pl-0 sm:table-cell hidden"
-                  >
-                    {" "}
-                    Başlık
-                  </th>
-                  <th
-                    scope="col"
-                    className="py-3.5 pl-4 text-center text-sm font-normal text-slate-700 sm:pl-6 md:pl-0 sm:table-cell hidden"
-                  >
-                    Fiyat
-                  </th>
-                  <th
-                    scope="col"
-                    className="py-3.5 pl-4 text-center text-sm font-normal text-slate-700 sm:pl-6 md:pl-0 sm:table-cell hidden"
-                  >
-                    Adet
-                  </th>
-                  <th
-                    scope="col"
-                    className="py-3.5 pl-4 text-end text-sm font-normal text-slate-700 sm:pl-6 md:pl-0 sm:table-cell hidden"
-                  >
-                    Toplam
-                  </th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr className="border-b border-slate-200">
-                  <td className="py-4 ">
-                    <img
-                      src="https://parade.com/.image/t_share/MTkwNTgxNDY1MzcxMTkxMTY0/different-types-of-apples-jpg.jpg"
-                      alt=""
-                      className="w-12 h-12 object-cover"
-                    />
-                  </td>
-                  <td className="py-4">
-                    <span className="font-medium">Elma</span>
-                  </td>
-                  <td className="py-4 text-center">
-                    <span>16.00₺</span>
-                  </td>
-                  <td className="py-4 text-center">
-                    <span>1</span>
-                  </td>
-                  <td className="py-4 text-end">
-                    <span>16.00₺</span>
-                  </td>
-                </tr>
-              </tbody>
-              <tfoot>
-                <tr>
-                  <th className="text-right pt-6" colSpan="4" scope="row">
-                    <span className="font-normal text-slate-700">
-                      Ara Toplam
-                    </span>
-                  </th>
-                  <th className="text-right pt-4" scope="row">
-                    <span className="font-normal text-slate-700">61₺</span>
-                  </th>
-                </tr>
-                <tr>
-                  <th className="text-right pt-4" colSpan="4" scope="row">
-                    <span className="font-normal text-slate-700">KDV</span>
-                  </th>
-                  <th className="text-right pt-4" scope="row">
-                    <span className="font-normal text-red-600">+4.88₺</span>
-                  </th>
-                </tr>
-                <tr>
-                  <th className="text-right pt-4" colSpan="4" scope="row">
-                    <span className="font-normal text-slate-700">Total</span>
-                  </th>
-                  <th className="text-right pt-4" scope="row">
-                    <span className="font-normal text-slate-700">65.88₺</span>
-                  </th>
-                </tr>
-              </tfoot>
-            </table>
-            <div className="py-9">
-              <div className="border-t pt-9 border-slate-200">
-                <p className="text-sm font-light text-slate-700">
-                  Ödeme koşulları 14 gündür. Paketlenmemiş Borçların Geç
-                  Ödenmesi Yasası 0000'e göre, serbest çalışanların bu süreden
-                  sonra borçların ödenmemesi durumunda 00.00 gecikme ücreti
-                  talep etme hakkına sahip olduklarını ve bu noktada bu ücrete
-                  ek olarak yeni bir fatura sunulacağını lütfen unutmayın.
-                  Revize faturanın 14 gün içinde ödenmemesi durumunda, vadesi
-                  geçmiş hesaba ek faiz ve %8 yasal oran artı %0,5 Bank of
-                  England tabanı olmak üzere toplam %8,5 uygulanacaktır.
-                  Taraflar Kanun hükümleri dışında sözleşme yapamazlar.
-                </p>
-              </div>
-            </div>
-          </div>
-        </article>
+          </article>
+        </div>
+      </section>
+      <div className="flex justify-end mt-4">
+        <Button type="primary" size="large">
+          Yazdır
+        </Button>
       </div>
-    </section>
-    <div className="flex justify-end mt-4">
-      <Button type="primary" size="large">Yazdır</Button>
-    </div>
-  </Modal>
+    </Modal>
   );
 };
 
