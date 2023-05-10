37053e50c384bde7e26e3783affbd8f13d679a79
Fatmanur Gebeşoğlu <76436684+FatmanurGebesoglu@users.noreply.github.com>
2023-05-10 14:03:30 +0300

    StatisticPage created

--- client/src/pages/StatisticPage.jsx
+++ client/src/pages/StatisticPage.jsx
@@ -6,8 +6,24 @@ const StatisticPage = () => {
       <Header />
       <div className="px-6">
         <h1 className="text-4xl font-bold text-center mb-4">İstatistiklerim</h1>
-        <div>
-            <h2 className="text-lg">Hoşgeldin <span className="text-green-700 font-bold text-xl">admin</span> </h2>
+        <div className="statistic-section">
+          <h2 className="text-lg">
+            Hoşgeldin{" "}
+            <span className="text-green-700 font-bold text-xl">admin</span>{" "}
+          </h2>
+            <div className="statistic-cards grid grid-cols-4 my-10">
+                <div className="card-item bg-gray-800 p-8 rounded-lg">
+                    <div className="flex gap-x-4">
+                        <div className="rounded-full bg-white w-16 h-16 p-3">
+                            <img src="images/user.png" alt="user" />
+                        </div>
+                        <div className="text-white">
+                            <p className="mb-2 text-lg fonst-medium text-gray-400">Toplam Müşteri</p>
+                            <p className="text-xl font-bold text-gray-200">7</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
       </div>
     </>
