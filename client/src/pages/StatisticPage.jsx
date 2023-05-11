57cba00951d655e926634b4d95870d5b182a4a31
Fatmanur Gebeşoğlu <76436684+FatmanurGebesoglu@users.noreply.github.com>
2023-05-11 11:22:27 +0300

    StatisticPage edited

--- client/src/pages/StatisticPage.jsx
+++ client/src/pages/StatisticPage.jsx
@@ -1,8 +1,8 @@
 import Header from "../components/Header/Header.jsx";
 import StatisticCard from "../components/Statistic/StatisticCard.jsx";
 import { useState, useEffect } from "react";
-import { Area } from '@ant-design/plots';
-import { Pie } from '@ant-design/plots';
+import { Area } from "@ant-design/plots";
+import { Pie } from "@ant-design/plots";
 
 const StatisticPage = () => {
   const [data, setData] = useState([]);
@@ -33,64 +33,64 @@ const StatisticPage = () => {
 
   const data2 = [
     {
-      type: '分类一',
+      type: "分类一",
       value: 27,
     },
     {
-      type: '分类二',
+      type: "分类二",
       value: 25,
     },
     {
-      type: '分类三',
+      type: "分类三",
       value: 18,
     },
     {
-      type: '分类四',
+      type: "分类四",
       value: 15,
     },
     {
-      type: '分类五',
+      type: "分类五",
       value: 10,
     },
     {
-      type: '其他',
+      type: "其他",
       value: 5,
     },
   ];
 
   const config2 = {
     appendPadding: 10,
-    data:data2,
-    angleField: 'value',
-    colorField: 'type',
+    data: data2,
+    angleField: "value",
+    colorField: "type",
     radius: 1,
     innerRadius: 0.6,
     label: {
-      type: 'inner',
-      offset: '-50%',
-      content: '{value}',
+      type: "inner",
+      offset: "-50%",
+      content: "{value}",
       style: {
-        textAlign: 'center',
+        textAlign: "center",
         fontSize: 14,
       },
     },
     interactions: [
       {
-        type: 'element-selected',
+        type: "element-selected",
       },
       {
-        type: 'element-active',
+        type: "element-active",
       },
     ],
     statistic: {
       title: false,
       content: {
         style: {
-          whiteSpace: 'pre-wrap',
-          overflow: 'hidden',
-          textOverflow: 'ellipsis',
+          whiteSpace: "pre-wrap",
+          overflow: "hidden",
+          textOverflow: "ellipsis",
         },
-        content: 'AntV\nG2Plot',
+        content: "AntV\nG2Plot",
       },
     },
   };
@@ -98,12 +98,12 @@ const StatisticPage = () => {
   return (
     <>
       <Header />
-      <div className="px-6">
+      <div className="px-6 md:pb-0 pb-20">
         <h1 className="text-4xl font-bold text-center mb-4">İstatistiklerim</h1>
         <div className="statistic-section">
           <h2 className="text-lg">
-            Hoşgeldin{" "}
-            <span className="text-green-700 font-bold text-xl">admin</span>{" "}
+            Hoş geldin{" "}
+            <span className="text-green-700 font-bold text-xl">admin</span>.
           </h2>
           <div className="statistic-cards grid xl:grid-cols-4 md:grid-cols-2 my-10 md:gap-10 gap-4">
             <StatisticCard
@@ -113,7 +113,7 @@ const StatisticPage = () => {
             />
             <StatisticCard
               title={"Toplam Kazanç"}
-              amount={"660.75 ₺"}
+              amount={"660.96 ₺"}
               img={"images/money.png"}
             />
             <StatisticCard
@@ -127,9 +127,13 @@ const StatisticPage = () => {
               img={"images/product.png"}
             />
           </div>
-          <div className="flex">
-            <div><Area {...config} /></div>
-            <div><Pie {...config2} /></div>
+          <div className="flex justify-between gap-10 lg:flex-row flex-col items-center">
+            <div className="lg:w-1/2 lg:h-full h-72">
+              <Area {...config} />
+            </div>
+            <div className="lg:w-1/2 lg:h-full h-72">
+              <Pie {...config2} />
+            </div>
           </div>
         </div>
       </div>
