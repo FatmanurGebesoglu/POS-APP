dd4c1709cd802b932445f76a4cffdf36b3f21d96
Fatmanur Gebeşoğlu <76436684+FatmanurGebesoglu@users.noreply.github.com>
2023-05-19 14:33:42 +0300

    Connected to MongoDB

--- api/server.js
+++ api/server.js
@@ -1,9 +1,23 @@
 const express = require("express");
-
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 const app = express();
-
 const port = 5000;
+
+dotenv.config();
+
+const connect = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI); //dotenv ile env dosyasındaki MONGO_URI'yi çekiyoruz
+        console.log("Connected to MongoDB");
+    } catch (err) {
+        console.log(err);
+    }
+};
 
 app.get("/", (req, res) => res.send("Hello World!"));
 
-app.listen(port, () => console.log(`Server started on port ${port}`));
+app.listen(port, () =>{
+    connect();
+    console.log(`Server started on port ${port}`)
+});
