b820cfe6402c8d686db284fb8f3ebb2fda7e38e1
Fatmanur Gebeşoğlu <76436684+FatmanurGebesoglu@users.noreply.github.com>
2023-05-19 22:27:23 +0300

    categories router created

---
+++ api/routes/categories.js
@@ -0,0 +1,16 @@
+const Category = require("../models/Category.js");
+const express = require("express");
+const router = express.Router();
+
+
+router.post("/", async (req, res) => {
+    try {
+        const newCategory = new Category(req.body);
+        await newCategory.save();
+        res.status(200).json("category has been created");
+    } catch (error) {
+        res.status(400).json(error);
+    }
+});
+
+module.exports = router;
