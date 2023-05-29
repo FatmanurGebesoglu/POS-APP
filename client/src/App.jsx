795fbb88d3f9574b9350036dca7e1a4630a5f6ec
Fatmanur Gebeşoğlu <76436684+FatmanurGebesoglu@users.noreply.github.com>
2023-05-29 14:03:18 +0300

    App file edited

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,30 +1,80 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import CartPage from "./pages/CartPage";
-import BillsPage from "./pages/BillsPage";
-import CustomersPage from "./pages/CustomersPage";
+import BillPage from "./pages/BillsPage";
+import CustomerPage from "./pages/CustomersPage";
 import StatisticPage from "./pages/StatisticPage";
 import ProductPage from "./pages/ProductPage";
 import Register from "./pages/auth/Register";
 import Login from "./pages/auth/Login";
 
-
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/cart" element={<CartPage />} />
-        <Route path="/bills" element={<BillsPage />} />
-        <Route path="/customers" element={<CustomersPage />} />
-        <Route path="/statistic" element={<StatisticPage />} />
-        <Route path="/products" element={<ProductPage />} />
+        <Route
+          path="/"
+          element={
+            <RouteControl>
+              <HomePage />
+            </RouteControl>
+          }
+        />
+        <Route
+          path="/cart"
+          element={
+            <RouteControl>
+              <CartPage />
+            </RouteControl>
+          }
+        />
+        <Route
+          path="/bills"
+          element={
+            <RouteControl>
+              <BillPage />
+            </RouteControl>
+          }
+        />
+        <Route
+          path="/customers"
+          element={
+            <RouteControl>
+              <CustomerPage />
+            </RouteControl>
+          }
+        />
+        <Route
+          path="/statistic"
+          element={
+            <RouteControl>
+              <StatisticPage />
+            </RouteControl>
+          }
+        />
+        <Route
+          path="/products"
+          element={
+            <RouteControl>
+              <ProductPage />
+            </RouteControl>
+          }
+        />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
   );
-} 
+}
 
 export default App;
+
+
+export const RouteControl = ({ children }) => {
+  if (localStorage.getItem("posUser")) {
+    return children;
+  } else {
+    return <Navigate to="/login" />;
+  }
+};
